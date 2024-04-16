d9cd97d5d6fc213fa631373ab5001a16cb5291de
Jose Everton <devjoseeverton@gmail.com>
2024-04-16 10:57:25 -0300

    xadrez e impressao do tabuleiro

--- src/tabuleiro/Piece.java
+++ src/tabuleiro/Piece.java
@@ -2,4 +2,15 @@ package tabuleiro;
 
 public class Piece {
 
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
 }
